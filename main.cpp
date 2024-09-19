186894429d8ff267136f575cc5bec280cc8b4546
Viktorija Taurinskaite <vika.taurinskait@gmail.com>
2024-09-19 14:47:14 +0300

    Pridetas Galutinio balo skaiciavimas pagal mediana

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::vector;
+using std::string;
 
 struct Studentas{
-    std::string vardas, pavarde;
-    std::vector<int> NamuDarbai;
+    string vardas, pavarde;
+    vector<int> NamuDarbai;
     int egz, rez;
 };
 
@@ -38,14 +40,28 @@ double vidurkioSkaic(const vector<int> &pazymiai){
     };
     return suma/static_cast<double>(pazymiai.size());
 };
-double skaicGalutiniBala(const Studentas &studentas){
+double medianosSkaic(vector<int> pazymiai){
+    std::sort(pazymiai.begin(), pazymiai.end());
+    int kiekis = pazymiai.size();
+    if (kiekis % 2 == 0){
+        return (pazymiai[kiekis / 2 - 1] + pazymiai[kiekis / 2]) / 2.0;
+    } else {
+        return pazymiai[kiekis /2];
+    }
+}
+double skaicGalutiniBalaVidur(const Studentas &studentas){
     double vidutinisND = vidurkioSkaic(studentas.NamuDarbai);
     return (vidutinisND * 0.4) + (studentas.egz * 0.6);
 };
+double skaicGalutiniBalaMed(const Studentas &studentas){
+    double medianaND = medianosSkaic(studentas.NamuDarbai);
+    return (medianaND * 0.4) + (studentas.egz * 0.6);
+}
 void spausdinimas(Studentas Lok){
     cout<< std::setw(15) << std::left << Lok.pavarde
         << std::setw(15) << std::left << Lok.vardas
-        << std::setw(20) << std::left << skaicGalutiniBala(Lok);
+        << std::setw(20) << std::left << std::fixed << std::setprecision(2) << skaicGalutiniBalaVidur(Lok)
+        << std::setw(20) << std::left << std::fixed << std::setprecision(2) << skaicGalutiniBalaMed(Lok) << endl;
 };
 int main()
 {
@@ -60,14 +76,13 @@ int main()
         Vec1.push_back(A);
         valymas(A);
     }
-
     cout << std::setw(15) << std::left << "Pavarde"
          << std::setw(15) << std::left << "Vardas"
-         << std::setw(20) << std::left << "Galutinis (Vid.)"<<endl;
-    for (int i = 0; i < 50; i++) {
-        cout << "-";
-    }
-    cout<<endl;
+         << std::setw(20) << std::left << "Galutinis (Vid.)"
+         << std::setw(20) << std::left << "Galutinis (Med.)" << endl;
+
+    cout << string(70, '-') << endl;
+
     for(int i=0; i<skaicius; i++){
         spausdinimas(Vec1.at(i));
         cout<<endl;
