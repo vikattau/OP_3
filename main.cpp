09a0a8b75b46e93ae2c5fd1bdc88c1dd3e3badb2
Viktorija Taurinskaite <vika.taurinskait@gmail.com>
2024-10-22 19:49:00 +0300

    pakoreguotas naudotojo pasirinkimas, patvarkytas main.cpp

--- main.cpp
+++ main.cpp
@@ -15,21 +15,55 @@ int main(){
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-    cout << "Kuri konteineri norite naudoti? \n"
+    if (ats == 1 || ats == 0 || ats == 3){
+        cout << "Kuri konteineri norite naudoti? \n"
         << "1 - vector\n"
         << "2 - list\n";
-    int konteineris;
-    while(!(cin >> konteineris) || (konteineris != 1 && konteineris != 2)){
-        cout << "Netinkama ivestis. Bandykite dar karta: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    if (konteineris == 1){
-        vector<Studentas> Vec1;
-        programosPasirinkimas(ats, Vec1);
-    } else if (konteineris == 2){
-        list<Studentas> List1;
-        programosPasirinkimas(ats, List1);
+        int konteineris;
+        while(!(cin >> konteineris) || (konteineris != 1 && konteineris != 2)){
+            cout << "Netinkama ivestis. Bandykite dar karta: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        if (konteineris == 1){
+            vector<Studentas> Vec1;
+            programosPasirinkimas(ats, Vec1);
+        } else if (konteineris == 2){
+            list<Studentas> List1;
+            programosPasirinkimas(ats, List1);
+        }
+    } else if (ats == 2){
+        cout << "Kiek studentu norite sugeneruoti? (iveskite atitinkama skaiciu): \n" <<
+                "1 - 1000 studentu \n" <<
+                "2 - 10000 studentu \n" <<
+                "3 - 100000 studentu \n" <<
+                "4 - 1000000 studentu \n" <<
+                "5 - 10000000 studentu \n" << endl;
+        int skaicius;
+        while(!(cin >> skaicius) || (skaicius != 1 && skaicius != 2 && skaicius != 3 && skaicius != 4 && skaicius != 5)){
+            cout << "Netinkama ivestis. Bandykite dar karta: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        string failoPav;
+        if (skaicius == 1){
+            failoPav = "studentai_1000.txt";
+            generuotiStudentus(1000, failoPav);
+        }else if (skaicius == 2){
+            failoPav = "studentai_10000.txt";
+            generuotiStudentus(10000, failoPav);
+        }else if (skaicius == 3){
+            failoPav = "studentai_100000.txt";
+            generuotiStudentus(100000, "studentai_100000.txt");
+        }else if (skaicius == 4) {
+            failoPav = "studentai_1000000.txt";
+            generuotiStudentus(1000000, failoPav);
+        }else if (skaicius == 5) {
+            failoPav = "studentai_10000000.txt";
+            generuotiStudentus(10000000, failoPav);
+        }else{
+            cout << "Klaida" << endl;
+        }
     }
     system("pause");
     return 0;
--- templates.h
+++ templates.h
@@ -12,96 +12,64 @@ void programosPasirinkimas(int ats, Container &studentai){
                 "2 - studentai10000.txt \n"<<
                 "3 - studentai100000.txt \n"<<
                 "4 - studentai1000000.txt \n"<<endl;
-            int failas;
-            cin >> failas;
-            string filename;
-            switch (failas) {
-                case 1: filename = "kursiokai.txt"; break;
-                case 2: filename = "studentai10000.txt"; break;
-                case 3: filename = "studentai100000.txt"; break;
-                case 4: filename = "studentai1000000.txt"; break;
-                default: cout << "Tokio failo nera" << endl; return;
-            }
-            skaitytiFaila(studentai, filename);
-            if constexpr (is_same<Container, vector<Studentas>>::value) {
-                sort(studentai.begin(), studentai.end(), rusiavimasPavarde);
-            } else if constexpr (is_same<Container, list<Studentas>>::value) {
-                studentai.sort(rusiavimasPavarde);
-            }
-            spausdinimas(studentai);
+        int failas;
+        cin >> failas;
+        string filename;
+        switch (failas) {
+            case 1: filename = "kursiokai.txt"; break;
+            case 2: filename = "studentai10000.txt"; break;
+            case 3: filename = "studentai100000.txt"; break;
+            case 4: filename = "studentai1000000.txt"; break;
+            default: cout << "Tokio failo nera" << endl; return;
+        }
+        skaitytiFaila(studentai, filename);
+        if constexpr (is_same<Container, vector<Studentas>>::value) {
+            sort(studentai.begin(), studentai.end(), rusiavimasPavarde);
+        } else if constexpr (is_same<Container, list<Studentas>>::value) {
+            studentai.sort(rusiavimasPavarde);
+        }
+        spausdinimas(studentai);
     }else if (ats == 0){
-            cout<<"Kiek studentu ivesite?: ";
-            int skaicius;
-            while (!(cin >> skaicius) || skaicius <= 0){
-                cout << "Netinkama ivesti, bandykite dar karta: ";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-            cin.ignore();
-            for(int i=0; i<skaicius; i++){
-                cout<<"Iveskite informacija: "<<endl;
-                Studentas A;
-                duomenuived(A);
-                studentai.push_back(A);
-                cout << "Studento objektas saugomas adresu: " << &studentai.back() << endl;
-                valymas(A);
-            }
-            if constexpr (is_same<Container, vector<Studentas>>::value) {
-                sort(studentai.begin(), studentai.end(), rusiavimasPavarde);
-            } else if constexpr (is_same<Container, list<Studentas>>::value) {
-                studentai.sort(rusiavimasPavarde);
-            }
-            spausdinimas(studentai);
+        cout<<"Kiek studentu ivesite?: ";
+        int skaicius;
+        while (!(cin >> skaicius) || skaicius <= 0){
+            cout << "Netinkama ivesti, bandykite dar karta: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        cin.ignore();
+        for(int i=0; i<skaicius; i++){
+            cout<<"Iveskite informacija: "<<endl;
+            Studentas A;
+            duomenuived(A);
+            studentai.push_back(A);
+            cout << "Studento objektas saugomas adresu: " << &studentai.back() << endl;
+            valymas(A);
+        }
+        if constexpr (is_same<Container, vector<Studentas>>::value) {
+            sort(studentai.begin(), studentai.end(), rusiavimasPavarde);
+        } else if constexpr (is_same<Container, list<Studentas>>::value) {
+            studentai.sort(rusiavimasPavarde);
+        }
+        spausdinimas(studentai);
 
-    }else if (ats == 2){
-            cout << "Kiek studentu norite sugeneruoti? (iveskite atitinkama skaiciu): \n" <<
-                "1 - 1000 studentu \n" <<
-                "2 - 10000 studentu \n" <<
-                "3 - 100000 studentu \n" <<
-                "4 - 1000000 studentu \n" <<
-                "5 - 10000000 studentu \n" << endl;
-            int skaicius;
-            while(!(cin >> skaicius) || (skaicius != 1 && skaicius != 2 && skaicius != 3 && skaicius != 4 && skaicius != 5)){
-                cout << "Netinkama ivestis. Bandykite dar karta: ";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-            string failoPav;
-            if (skaicius == 1){
-                failoPav = "studentai_1000.txt";
-                generuotiStudentus(1000, failoPav);
-            }else if (skaicius == 2){
-                failoPav = "studentai_10000.txt";
-                generuotiStudentus(10000, failoPav);
-            }else if (skaicius == 3){
-                failoPav = "studentai_100000.txt";
-                generuotiStudentus(100000, "studentai_100000.txt");
-            }else if (skaicius == 4) {
-                failoPav = "studentai_1000000.txt";
-                generuotiStudentus(1000000, failoPav);
-            }else if (skaicius == 5) {
-                failoPav = "studentai_10000000.txt";
-                generuotiStudentus(10000000, failoPav);
-            }else{
-                cout << "Klaida" << endl;
-            }
-        }else if (ats == 3){
-            cout << "Kaip norite rusiuoti studentus?: \n"
-                << "1 - pagal pavardes \n"
-                << "2 - pagal galutini bala mazejancia tvarka \n"
-                << "3 - pagal galutini bala didejancia tvarka \n" << endl;
-            int rusiavimoPasirinkimas;
-            cin >> rusiavimoPasirinkimas;
+    } else if (ats == 3){
+        cout << "Kaip norite rusiuoti studentus?: \n"
+            << "1 - pagal pavardes \n"
+            << "2 - pagal galutini bala mazejancia tvarka \n"
+            << "3 - pagal galutini bala didejancia tvarka \n" << endl;
+        int rusiavimoPasirinkimas;
+        cin >> rusiavimoPasirinkimas;
 
-            vector<string> testuojamiFailai = {
+        vector<string> testuojamiFailai = {
             "studentai_1000.txt",
             "studentai_10000.txt",
             "studentai_100000.txt",
             "studentai_1000000.txt",
             "studentai_10000000.txt"
             };
-            for (const auto& failoPav : testuojamiFailai) {
-                cout << "Testuojamas failas: " << failoPav << endl;
+        for (const auto& failoPav : testuojamiFailai) {
+            cout << "Testuojamas failas: " << failoPav << endl;
 //                failuTestavimas(failoPav, Vec1, rusiavimoPasirinkimas);
             }
         }
